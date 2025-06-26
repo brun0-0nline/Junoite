92e9328be5daaa77cf90c5c4460ad93c375a69cf
brun0-0nline <brun0.0nline@pm.me>
2025-06-26 05:39:03 -0300

    Customize base image and attempt snapd

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,51 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install dnf5 if not installed
+if ! rpm -q dnf5 >/dev/null; then
+    rpm-ostree install dnf5
+fi
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+### Remove packages
+dnf5 -y remove firefox \
+               firefox-langpacks \
+               filelight \
+               kjournald \
+               kjournald-libs \
+               kfind \
+               khelpcenter \
+               krfb \
+               krfb-libs \
+               kf6-kquickcharts-6.15.0-1.fc42.x86_64 \
+               kcharselect \
+               kwrite \
+               plasma-discover-rpm-ostree
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+### Post removal cleanup
+dnf5 -y autoremove
+dnf5 clean all
+
+### Install DNF5 plugins to get the config-manager
+dnf5 -y in dnf5-plugins
+
+# Add rpmfusion
+dnf5 -y in distribution-gpg-keys && \
+rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-fedora-$(rpm -E %fedora) && \
+rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-nonfree-fedora-$(rpm -E %fedora) && \
+dnf5 -y --setopt=localpkg_gpgcheck=1 in https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+										https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+### Install packages for codecs and hardware acceleration
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
+dnf5 -y in libva-intel-driver
+
+# Installs snapd and create symlink to make it work (hopefully)
+dnf5 -y in snapd
+ln -s /var/lib/snapd/snap /snap
+snap set system homedirs=/var/home
+
+# Install distrobox
+dnf5 -y in distrobox
 
 # Use a COPR Example:
 #
